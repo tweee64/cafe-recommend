30141d3dd696e1b95386a975941a9b2de81946be
Thuy Pham Thu <twee64@Thuys-MacBook-Air.local>
2024-12-11 13:15:23 +0800

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,6 @@ import Home from "./pages/Home";
 import FilterData from "./pages/FilterData";
 import CafeDetail from "./pages/CafeDetail";
 import AddCafe from "./pages/AddCafe";
-import AddReviewConfirm from "./pages/AddReviewConfirm";
 function App() {
   return (
     <>
